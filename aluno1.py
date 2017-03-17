b15e14c312ce5554a8d06988b80984e08ccbcc88
gabrielvf1 <vf_gabriel@yahoo.com.br>
2017-03-17 11:41:15 -0300

    a

--- aluno1.py
+++ aluno1.py
@@ -1,7 +1,7 @@
 def ler_numeros(n):
 	lista=[]
 	for i in range(n):
-		a=int(input("Qual numero voce quer na lista agora?"))
+		a=input("Qual numero voce quer na lista agora?")
 		lista.append(a)
 	return lista
 
